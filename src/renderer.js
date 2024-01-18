93ba499462d3ed71ea17725c848a0544dda52bbc
Nirvik Saha <nsdev404@gmail.com>
2024-01-17 19:28:04 -0500

    dynamic search on select object function implemented

--- src/renderer.js
+++ src/renderer.js
@@ -7,8 +7,9 @@ var DataReceived = false;
 var DATA;
 var positionArr = [];
 var indexArr = [];
-var meshArr = [];
+var MESH_ARR = [];
 var dataArr = [];
+var SELECTED_MESH_ARR = [];
 
 var readClipboardBtn = document.getElementById("readClipboardBtn");
 var clearClipboardBtn = document.getElementById("clearClipboardBtn");
@@ -57,15 +58,6 @@ const createScene = async () => {
     ground.material = mat3;
 
     //
-    var mat1 = new BABYLON.StandardMaterial("mat1", scene);
-    mat1.diffuseColor = new BABYLON.Color3(1, 1, 0);
-
-    //
-    const selectMat = new BABYLON.StandardMaterial();
-    selectMat.diffuseColor = new BABYLON.Color3(1, 0, 0);
-    selectMat.specularColor = new BABYLON.Color3(1, 1, 0);
-
-    //
     var light = new BABYLON.HemisphericLight(
         "light2",
         new BABYLON.Vector3(10, 10, 10),
@@ -73,12 +65,38 @@ const createScene = async () => {
     );
 
     //
+    await readClipboardFunction(scene);
+    await selectObjects(scene);
+    // await showSearchProperties();
+
+    console.log("selected mesh:", SELECTED_MESH_ARR.length);
+
+    //
     scene.registerBeforeRender(() => {});
 
+    //
+    return scene;
+};
+
+async function showSearchProperties() {
+    let x = document.getElementById("searchdiv");
+    if (SELECTED_MESH_ARR.length > 0) {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+async function readClipboardFunction(scene) {
+    //
+    var mat1 = new BABYLON.StandardMaterial("mat1", scene);
+    mat1.diffuseColor = new BABYLON.Color3(1, 1, 0);
+    //
+    MESH_ARR = [];
+    SELECTED_MESH_ARR = [];
+    //
     readClipboardBtn.addEventListener("click", async () => {
         const res = await api.readClipboard();
-        //
-        // console.log("res in renderer: ", res);
         var count = 0;
         api.readClipboard()
             .then((data) => {
@@ -115,58 +133,65 @@ const createScene = async () => {
                     vertexData.normals = normals;
                     vertexData.applyToMesh(customMesh, true);
                     customMesh.material = mat1;
-                    meshArr.push(customMesh);
+                    MESH_ARR.push(customMesh);
                     dataArr.push(obj.property_set);
                     count++;
                 });
             })
-            .catch((err) => console.log(err));
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+}
+
+async function selectObjects(scene) {
+    //
+    var mat1 = new BABYLON.StandardMaterial("mat1", scene);
+    mat1.diffuseColor = new BABYLON.Color3(1, 1, 0);
+
+    //
+    const selectMat = new BABYLON.StandardMaterial();
+    selectMat.diffuseColor = new BABYLON.Color3(1, 0, 0);
+    selectMat.specularColor = new BABYLON.Color3(1, 1, 0);
+
+    scene.onPointerDown = async function castray() {
+        const hit = scene.pick(scene.pointerX, scene.pointerY);
+        var n = MESH_ARR.length;
+        let i = 0;
+        let reqName = "";
+        //
+        var div = document.getElementById("infodiv");
+        while (div.hasChildNodes()) {
+            div.firstChild.remove();
+        }
+        while (i < n) {
+            let name = "mesh" + i;
+            if (hit.pickedMesh && hit.pickedMesh.name == name) {
+                reqName = name;
+                dataArr[i].forEach((e) => {
+                    var s = e.param + " : " + e.value;
+                    var p = document.createElement("p");
+                    p.innerHTML = s;
+                    div.appendChild(p);
+                });
+            }
+            i++;
+        }
 
         //
-        // info.innerHTML = res;
-        scene.onPointerDown = function castray() {
-            const hit = scene.pick(scene.pointerX, scene.pointerY);
-            var n = meshArr.length;
-            let i = 0;
-            let reqName = "";
-            //
-            var div = document.getElementById("infodiv");
-            while (div.hasChildNodes()) {
-                div.firstChild.remove();
+        let tempSelectedArr = [];
+        MESH_ARR.forEach((e) => {
+            if (e.name === reqName) {
+                e.material = selectMat;
+                tempSelectedArr.push(e);
+            } else {
+                e.material = mat1;
             }
-            // var txa = document.getElementById("infotxt");
-            // var txa = document.getElementById("infotxt");
-            // txa.value = "";
-            while (i < n) {
-                let name = "mesh" + i;
-                if (hit.pickedMesh && hit.pickedMesh.name == name) {
-                    reqName = name;
-                    dataArr[i].forEach((e) => {
-                        var s = e.param + " : " + e.value;
-                        var p = document.createElement("p");
-                        p.innerHTML = s;
-                        div.appendChild(p);
-                        //  txa.append(s);
-                    });
-                    // console.log(dataArr[i]);
-                }
-
-                i++;
-            }
-
-            //
-            meshArr.forEach((e) => {
-                if (e.name === reqName) {
-                    e.material = selectMat;
-                } else {
-                    e.material = mat1;
-                }
-            });
-        };
-    });
-
-    return scene;
-};
+        });
+        SELECTED_MESH_ARR = [...tempSelectedArr];
+        await showSearchProperties();
+    };
+}
 
 const scene = await createScene();
 
